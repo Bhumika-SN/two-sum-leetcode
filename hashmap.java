263e55a570ab70ff69fe06e77d1911ff9839c704
Bhumika SN <bhumikasnn.dev@gmail.com>
2026-01-23 22:52:14 +0530

    Update hashmap.java

--- hashmap.java
+++ hashmap.java
@@ -5,6 +5,8 @@ public class HashMap {
     public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> seen = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
+
+{ 
             int need = target - nums[i];
             if (seen.containsKey(need)) {
                 return new int[] {seen.get(need), i};
